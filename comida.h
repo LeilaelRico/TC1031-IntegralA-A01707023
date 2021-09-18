20bd19e94d04c56e3d8cdc5b23bb47b550baf524
Leilael Rico <a01707023@itesm.mx>
2021-09-18 18:29:35 -0500

    Se comienza a trabajar en la clase comida / Se crea el main.

---
+++ comida.h
@@ -0,0 +1,35 @@
+/*
+
+comida.h
+
+ITESM
+Campus Querétaro
+Programación de Estructuras de Datos y Algoritmos Fundamentales.
+
+Cristian Leilael Rico Espinosa.
+Matrícula: A01707023.
+*/
+
+#ifndef COMIDA_H_
+#define COMIDA_H_
+
+#include <iostream>
+using namespace std;
+
+class Comida {
+
+    private:
+
+        string nombre;
+        int calorias;
+        float carbohidratos;
+        float lipidos;
+        float proteina;
+
+    public:
+        Comida(){};
+        Comida(string, int, float, float, float){};
+
+};
+
+#endif /* COMIDA_H_ */
---
+++ main.cpp
@@ -0,0 +1,11 @@
+/*
+
+main.cpp
+
+ITESM
+Campus Querétaro
+Programación de Estructuras de Datos y Algoritmos Fundamentales.
+
+Cristian Leilael Rico Espinosa.
+Matrícula: A01707023.
+*/
