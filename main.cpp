246a1fac6a933f91f3734361881f0feea1262dff
Leilael Rico <a01707023@itesm.mx>
2021-09-19 18:23:28 -0500

    Se comienza el proceso de ordenamiento.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,10 @@ Matrícula: A01707023.
 #include "sorts.h"
 #include <fstream>
 #include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstring>
 using namespace std;
 
 
@@ -27,6 +31,19 @@ void menu()
 }
 
 
+template <class T>
+string arrayToString(const vector<T> &v) {
+	stringstream aux;
+
+	aux << "[" << v[0];
+	for (int i = 1; i < v.size(); i++) {
+		aux << ", " << v[i];
+	}
+	aux << "]";
+	return aux.str();
+}
+
+
 int main(){
 
     ifstream myfile("datost.txt");
@@ -35,55 +52,72 @@ int main(){
     float cb, lp, pt;
     vector<Comida> comid;
     Comida c;
+    vector<string> vc;
+    string arr[27];
 
-    
-  do{
-    menu();
-    cout<<"Elige una de las opciones: ";
-    cin>>opc;
+    do{
+        menu();
+        cout<<"Elige una de las opciones: ";
+        cin>>opc;
 
-        switch(opc){
-            case 1: 
-                while(myfile.peek()!=EOF)
-                {
-
-                    if(myfile>>nam>>cl>>cb>>lp>>pt)
+            switch(opc){
+                case 1:
+                    while(myfile.peek()!=EOF)
                     {
-                        c.setNombre(nam);      
-                        c.setCalorias(cl);
-                        c.setCarbohidratos(cb);
-                        c.setLipidos(lp);
-                        c.setProteinas(pt);
-                        comid.push_back(c);
+
+                        if(myfile>>nam>>cl>>cb>>lp>>pt)
+                        {
+                            c.setNombre(nam);
+                            c.setCalorias(cl);
+                            c.setCarbohidratos(cb);
+                            c.setLipidos(lp);
+                            c.setProteinas(pt);
+                            comid.push_back(c);
+                        }
                     }
-                }
-                myfile.close();
-                cout<<"Datos Cargados"<<endl;
+                    myfile.close();
+                    cout<<"Datos Cargados"<<endl;
+                        break;
+
+
+                case 2:
+                    for (i = 0; i<comid.size(); i++){
+                        cout<<"\n--------------------------------";
+                            cout<<"\nNombre: "<<comid[i].getNombre();
+                            cout<<"\nCalorias: "<<comid[i].getCalorias();
+                            cout<<"\nCarbohidratos: "<<comid[i].getCarbohidratos();
+                            cout<<"\nLipidos: "<<comid[i].getLipidos();
+                            cout<<"\nProteinas: "<<comid[i].getProteinas();
+                            cout<<"\n--------------------------------";
+                    }
                     break;
 
 
-            case 2:
-                for (i = 0; i<comid.size(); i++){
-                    cout<<"\n--------------------------------";
-                        cout<<"\nNombre: "<<comid[i].getNombre();
-                        cout<<"\nCalorias: "<<comid[i].getCalorias();
-                        cout<<"\nCarbohidratos: "<<comid[i].getCarbohidratos();
-                        cout<<"\nLipidos: "<<comid[i].getLipidos();
-                        cout<<"\nProteinas: "<<comid[i].getProteinas();
-                        cout<<"\n--------------------------------";
-                }
-                break;
+                case 3:
+
+                    for (i = 0; i<comid.size(); i++){
+                        string arr[i] = {comid[i].getNombre()};
+                    }
+
+                    vector<string> original1 (arr, arr + sizeof(arr) / sizeof(int) );
+
+                    vector<string> prueba;
+                    Sorts<string> sorts;
+
+                    prueba = original1;
+                    sorts.ordenaSeleccion(prueba);
+
+                    cout << "\n programa " << arrayToString(prueba) << "\n";
 
 
-            case 3:
-                cout<<"Op 3"<<endl;
-                break;
 
-                
+                    break;
+
+
+            }
         }
-    } 
-    while(opc != 4);
-    cout<<"Salida\n";
+        while(opc != 4);
+        cout<<"Salida\n";
 
 
     return 0;
