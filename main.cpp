9cbbd0094d984346c12ea6ed3bbc5f4883064dcf
Leilael Rico <a01707023@itesm.mx>
2021-10-30 19:26:58 -0500

    Se integran los avances anteriores.

--- main.cpp
+++ main.cpp
@@ -13,9 +13,13 @@ Matrícula: A01707023.
 #include <list>
 #include <iterator>
 #include <string>
+#include <sstream>
+#include <cstring>
+
 
 #include "sorts.h"
 #include "list.h"
+#include "comida.h"
 
 using namespace std;
 
@@ -31,12 +35,39 @@ void menu()
 }
 
 
+void menu1()
+{
+    cout<<"\n--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**"<<endl;
+    cout<<"1. Leer los datos."<<endl;
+    cout<<"2. Mostrar los datos."<<endl;
+    cout<<"3. Ordenar los nombres."<<endl;
+    cout<<"4. Salir."<<endl;
+    cout<<"--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**"<<endl;
+}
+
+
+template <class T>
+string arrayToString(const vector<T> &v) {
+	stringstream aux;
+
+	aux << "0 " << v[0];
+	for (int i = 1; i < v.size(); i++) {
+		aux << "\n" << i << " " << v[i];
+	}
+
+	return aux.str();
+}
+
+
 int main(){
-    int op = 0, op1 = 0;
+
+
+    int op = 0;
     int el;
     List<string>lscomid;
     string str, comList, nom, cal, car, li, pro, neoCom;
     ifstream myfile ("datost.txt");
+
 
     do{
     menu();
@@ -135,6 +166,84 @@ int main(){
                  else{
                    cout<<"\nError al crear un archivo.\n";
                  }
+
+
+    /************************* Uso de clase y ejecución de segundo menú *************************/
+
+
+    ifstream ficla("datosed.txt");
+    string nam;
+    int cl, i, opc;
+    float cb, lp, pt;
+    vector<Comida> comid;
+    Comida c;
+    vector<string> vc;
+    string arr[28];
+
+    do{
+        menu1();
+        cout<<"Elige una de las opciones: ";
+        cin>>opc;
+
+            switch(opc){
+                case 1:
+                    while(ficla.peek()!=EOF)
+                    {
+
+                        if(ficla>>nam>>cl>>cb>>lp>>pt)
+                        {
+                            c.setNombre(nam);
+                            c.setCalorias(cl);
+                            c.setCarbohidratos(cb);
+                            c.setLipidos(lp);
+                            c.setProteinas(pt);
+                            comid.push_back(c);
+                        }
+                    }
+                    ficla.close();
+                    cout<<"Datos Cargados"<<endl;
+                        break;
+
+
+                case 2:
+                    for (i = 0; i<comid.size(); i++){
+                        cout<<"\n--------------------------------";
+                            cout<<"\nNombre: "<<comid[i].getNombre();
+                            cout<<"\nCalorias: "<<comid[i].getCalorias();
+                            cout<<"\nCarbohidratos: "<<comid[i].getCarbohidratos();
+                            cout<<"\nLipidos: "<<comid[i].getLipidos();
+                            cout<<"\nProteinas: "<<comid[i].getProteinas();
+                            cout<<"\n--------------------------------";
+                    }
+                    break;
+
+
+                case 3:
+
+                    for (i = 0; i<comid.size(); i++){
+                        arr[i] = {comid[i].getNombre()};
+                    }
+
+                    for (i = 0; i<comid.size(); i++){
+                        cout<< i<<" "<<arr[i]<<endl;
+                    }
+
+
+                    vector<string> originalnom (arr, arr + sizeof(arr) / sizeof(string) );
+
+                    vector<string> orcom;
+                    Sorts<string> sorts;
+
+                    orcom = originalnom;
+                    sorts.ordenaMerge(orcom);
+
+                    cout << "\nOrdenado:\n" << arrayToString(orcom) << "\n";
+
+                    break;
+            }
+        }
+        while(opc != 4);
+        cout<<"Salida\n";
 
 
     return 0;
